ab15f009d377a8310a1df799ac9f3cae14dbf849
VladimirZhd <zhd18001@byui.edu>
2021-08-28 11:31:09 -0600

    #17 server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const config = require('config');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || config.get('PORT');
+
+const app = express();
+
+connectDB();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('port', PORT);
+
+app.listen(PORT, () => `Server is running on port: ${PORT}`);
