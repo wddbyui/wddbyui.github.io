3a7d896866da7da10160233b735af78517a4c787
VladimirZhd <zhd18001@byui.edu>
2021-09-04 14:12:45 -0600

    adede server stuff

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const config = require('config');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || config.get('PORT');
+
+const app = express();
+const routes = require('./backend/routes');
+
+connectDB();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
+
+app.set('port', PORT);
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
