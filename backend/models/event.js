30a43ee8e47834a7f28f882f12620d32fe37226f
erikqb3 <erikqb3@gmail.com>
2021-11-10 16:44:31 -0700

    event.js event models.

---
+++ backend/models/event.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema ({
+  eventName: { type: String, required: true},
+  eventType: { type: String, required: false},
+
+  startDate: {type: Date, required: true},
+  startTime_hour: {type: Number, required: true},
+  startTime_min: {type: Number, required: true},
+  startTime_AmPm: {type: String, required: true},
+
+  endDate: {type: Date, required: false},
+  endTime_hour: {type: Number, required: false},
+  endTime_min: {type: Number, required: false},
+  endTime_AmPm: {type: String, required: false},
+
+  eventLocation: {type: String, required: true},
+
+  eventRepeat: {type: Boolean, requried: false},
+
+
+  description: { type: String, required}
+
+})
